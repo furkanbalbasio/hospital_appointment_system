a6b65a2d2aa87a3345ad5d86f4661dfec5c5ea4e
furkan <fuurkan_9638@hotmail.com>
2024-06-20 18:01:40 +0300

    Hospital Appointment System

---
+++ PatienceService/src/main/java/org/has/PatienceServiceApplication.java
@@ -0,0 +1,11 @@
+package org.has;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatienceServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PatienceServiceApplication.class);
+    }
+}
---
+++ SecretaryService/src/main/java/org/has/SecretaryServiceApplication.java
@@ -0,0 +1,11 @@
+package org.has;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretaryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecretaryServiceApplication.class);
+    }
+}
