4a6b786d1f837ca1909a59dfc342db095f5bc1a3
ugur <ayhan.ugur.celik@gmail.com>
2024-07-16 17:43:39 +0300

    admin token duration raised 60min

--- PatienceService/src/main/java/org/has/controller/PatienceController.java
+++ PatienceService/src/main/java/org/has/controller/PatienceController.java
@@ -3,10 +3,10 @@ package org.has.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.has.dto.request.PatienceSaveRequestDto;
-import org.has.dto.response.DoctorResponseDto;
+
 import org.has.repository.entity.Patience;
 import org.has.service.PatienceService;
-import org.has.utility.enums.EDepartment;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,8 +25,5 @@ public class PatienceController {
         Patience user =   patienceService.save(dto);
         return ResponseEntity.ok().build();
     }
-    @GetMapping("/find-by-department-doctor")
-    public ResponseEntity<List<DoctorResponseDto>> findByDepartment(@RequestParam("department") EDepartment department){
-        return ResponseEntity.ok(patienceService.findByDepartment(department));
-    }
+
 }
--- PatienceService/src/main/java/org/has/service/PatienceService.java
+++ PatienceService/src/main/java/org/has/service/PatienceService.java
@@ -2,12 +2,9 @@ package org.has.service;
 
 import lombok.RequiredArgsConstructor;
 import org.has.dto.request.PatienceSaveRequestDto;
-import org.has.dto.response.DoctorResponseDto;
-import org.has.manager.DoctorManager;
 import org.has.mapper.PatienceMapper;
 import org.has.repository.PatienceRepository;
 import org.has.repository.entity.Patience;
-import org.has.utility.enums.EDepartment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,13 +13,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PatienceService {
     private final PatienceRepository patienceRepository;
-    private final DoctorManager doctorManager;
+
     public Patience save(PatienceSaveRequestDto dto) {
         Patience patience=patienceRepository.save(PatienceMapper.INSTANCE.fromDto(dto));
         return patience;
     }
-    public List<DoctorResponseDto> findByDepartment(EDepartment department){
-       return doctorManager.findByDepartment(department).getBody();
-    }
+
     
 }
